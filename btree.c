16e55d12bd367c94fdf5f4d89ab0f272888138c8
Idris Boudour <idrisbr52@gmail.com>
2024-11-14 10:49:15 +0100

    Jour 2 : Ajout des fonctions de création et libération de la table

--- btree.c
+++ btree.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,6 +6,9 @@
 
 Table* create_table() {
     Table* table = (Table*)malloc(sizeof(Table));
+    if (table == NULL) {
+        return NULL;
+    }
     table->root = NULL;
     table->next_id = 1;  
     return table;
@@ -18,11 +22,12 @@ void free_tree(Node* node) {
 }
 
 void free_table(Table* table) {
+    if (table == NULL) return;
     free_tree(table->root);
     free(table);
 }
 
-void insert_row(Table* table, Row* row) { }
-void select_rows(Table* table) { }
-void update_row(Table* table, int id, const char* new_username, const char* new_email) { }
+void insert_row(Table* table, Row* row) {  }
+void select_rows(Table* table) {  }
+void update_row(Table* table, int id, const char* new_username, const char* new_email) {  }
 void delete_row(Table* table, int id) {  }
--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+// main.c
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "btree.h"
@@ -5,7 +7,16 @@
 
 int main() {
     Table* table = create_table();  
+    if (table == NULL) {
+        printf("Erreur lors de la création de la table.\n");
+        return 1;
+    }
+    printf("Table créée avec succès.\n");
+
     repl();  
+
     free_table(table);  
+    printf("Table libérée avec succès.\n");
+
     return 0;
 }
